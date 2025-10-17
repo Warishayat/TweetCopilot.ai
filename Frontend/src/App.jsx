51a88c9ed981a2fabfa076ee91bf8bcde4e2beaa
waris hayat <warishayat666@gmail.com>
2025-10-17 23:25:35 +0500

    Frontend

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div>
+        <h2  className="text-3xl font-bold underline text-center mt-10 bg-zinc-600">my name is waris</h2>
+      </div>
+    </>
+  )
+}
+
+export default App
